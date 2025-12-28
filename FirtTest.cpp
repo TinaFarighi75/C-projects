e0d0d0ba7b8f7bfef4fbe2d4a8458d197e079903
Tina Farighi <TinaFarighi75@gmail.com>
2025-12-28 18:53:30 +0330

    feat:add first c++ project

---
+++ FirtTest.cpp
@@ -0,0 +1,13 @@
+// title : my first project of C++
+// Author : Tina Farighi
+// date : 1404/10/07
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Welcome to my C++ project." << endl;
+    cout << "My Name is : \n Tina :)";
+    return 0;
+}
