6e1cdca99f7e1548a93a0627458ce1ea35297e85
Tina Farighi <TinaFarighi75@gmail.com>
2025-12-29 12:41:56 +0330

    feat: add new projet : first Letter of my name

---
+++ firstLetterName.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    for (int i = 0; i < 17; i++) {
+        cout << "*";
+    }
+    cout << endl;
+
+
+    for (int i = 0; i < 7; i++) {
+        cout << "\t*" << endl;
+    }
+
+    return 0;
+}
